e535df5c0fa59b86bc8f8cf72a9b7aca5d46e34c
Juan Pablo Carvajal Abrahams <jcarvajalabrahams@gmail.com>
2024-04-13 13:39:49 -0600

    feat: add the test cases

--- test/tester.v
+++ test/tester.v
@@ -7,12 +7,15 @@ module tester (
 	gate_o,
 	gate_cls,
 	alm_pin,
-	alm_blkg
+	alm_blkg,
+	ent_pin
 );
 
-output reg clock, reset, senr_e, senr_x;
+output reg clock, reset, senr_e, senr_x, ent_pin;
 output reg [7:0] pin;
 input gate_o, gate_cls, alm_pin, alm_blkg;
+
+always #5 clock = !clock;
 
 initial begin
 	clock = 0;
@@ -20,20 +23,101 @@ initial begin
 	senr_e = 0;
 	senr_x = 0;
 	pin = 0;
+	ent_pin = 0;
 	#5 reset = 1;
 	#5 reset = 0;
 
 	// Prueba #1
 	#10 senr_e = 1;
-	#10 pin = 8'd71;
+	
+	// Ingreso de Pin correcto 
+	#10 pin = 8'd72;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	
 	#10 senr_e = 0;
 	#10 senr_x = 1;
-
-
-end
-
-always begin
-	#5 clock = !clock;
+	#10 senr_x = 0;
+	
+	// Prueba #2
+	
+	#20 senr_e = 1;
+	
+	// Ingreso de Pin incorrecto 
+	#10 pin = 8'd74;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	
+	// Ingreso de Pin incorrecto 
+	#10 pin = 8'd22;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	
+	// Ingreso de pin correcto 
+	#10 pin = 8'd72;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	
+	#10 senr_e = 0;
+	#10 senr_x = 1;
+	#10 senr_x = 0;
+	
+	// Prueba #3
+	#20 senr_e = 1;
+	
+	// Ingreso de Pin incorrecto 
+	#10 pin = 8'd74;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	
+	// Ingreso de Pin incorrecto 
+	#10 pin = 8'd22;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	
+	// Ingreso de Pin incorrecto 
+	#10 pin = 8'd36;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	
+	// Ingreso de Pin incorrecto 
+	#10 pin = 8'd42;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	
+	// Ingreso de pin correcto 
+	#10 pin = 8'd72;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	
+	#10 senr_e = 0;
+	#10 senr_x = 1;
+	#10 senr_x = 0;
+	
+	// Prueba #4
+	#10 senr_e = 1;
+	
+	// Ingreso de Pin correcto 
+	#10 pin = 8'd72;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	
+	// Se bloquea compuerta
+	#10 senr_x = 1;
+	
+	// Ingreso de Pin incorrecto 
+	#10 pin = 8'd22;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	
+	// Ingreso de Pin correcto 
+	#10 pin = 8'd72;
+	#5 ent_pin = 1;
+	#5 ent_pin = 0;
+	#10 senr_x = 0;
+	
+	
+	#30 $finish;
 end
 
 endmodule
